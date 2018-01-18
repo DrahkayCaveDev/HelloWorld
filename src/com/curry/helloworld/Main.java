6fecea78a329b3bbdd8825b0d9531586aaca0283
DrahkayCaveDev <Dcurry2000@gmail.com>
2018-01-18 13:47:16 -0500

    Initial commit

---
+++ src/com/curry/helloworld/Main.java
@@ -0,0 +1,11 @@
+package com.curry.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 21;
+        while( x < 20 ) {
+            System.out.println("Do you know da wae");
+        }
+    }
+}
